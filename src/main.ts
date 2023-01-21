77af4608bb1e0ba3d648b0e4c32d3a9c8201411f
Tarsis de Lima <br.tmvdl@gmail.com>
2023-01-20 21:58:50 -0300

    Fixing main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+// import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const path: string = core.getInput('path')
+    core.debug(`Path: ${path}`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // core.debug(`Waiting ${ms} milliseconds ...`) 
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
+
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
